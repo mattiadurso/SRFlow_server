155992e66b881508f9e0da12387568eb54bbe861
mattiadurso <mattiadurso98@gmail.com>
2022-08-10 11:10:00 +0200

    .

--- run_jupyter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e # exit script if an error occurs
-
-source myenv/bin/activate # Install with ./setup.sh
-cd code
-python -m jupyter notebook demo_on_pretrained.ipynb # Start jupyter using the python from the virtual environment
